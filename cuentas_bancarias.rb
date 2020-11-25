cccd5439bfdf41f08928cdb2fc9cf65c155f5748
Jorge <jorgesifontes.cl@gmail.com>
2020-11-24 23:05:44 -0300

    Beta cuentas_bancarias.rb

---
+++ cuentas_bancarias.rb
@@ -0,0 +1,60 @@
+#Ejercicio 1
+class CuentaBancaria
+    attr_accessor :saldo
+    attr_reader :banco, :numero_de_cuenta
+
+    def initialize(banco, numero_de_cuenta, saldo = 0.0)
+        @banco = banco
+        #aplicar que el numero de cuenta sea valido
+        @numero_de_cuenta = numero_de_cuenta
+        @saldo = saldo
+    end
+
+    #añadir condicion sobre saldos negativos
+    def transferir(monto, cuenta)
+        @saldo -= monto
+        cuenta.saldo += monto
+        monto
+    end
+end
+
+class Usuario
+    attr_reader :nombre, :cuentas
+
+    def initialize(nombre, cuentas)
+        @nombre = nombre
+        @cuentas = []
+
+        if (cuentas.class != Array)
+            @cuentas.push(cuentas)
+        else
+            cuentas.each do |cuenta|
+                @cuentas.push(cuenta)
+            end
+        end
+    end
+
+    def saldo_total()
+        @cuentas.inject(0) do |total, cuenta|
+            total += cuenta.saldo
+        end
+        
+    end
+end
+
+cuenta1 = CuentaBancaria.new('Elite',12345,5000)
+cuenta2 = CuentaBancaria.new('Pro',6789,5000)
+cuenta3 = CuentaBancaria.new('Diamond',765456,5000)
+
+user1 = Usuario.new('Jorge',cuenta1)
+user2 = Usuario.new('Miguel',cuenta2)
+
+puts user1.saldo_total
+puts user2.saldo_total
+
+puts "Transferencia"
+
+user1.cuentas[0].transferir(user1.saldo_total, user1.cuentas[0])
+
+puts user1.saldo_total
+puts user2.saldo_total
