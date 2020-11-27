2ef7ebb05c2701b84d7acde807db2fffa646a7df
Jorge <jorgesifontes.cl@gmail.com>
2020-11-26 22:21:03 -0300

    Showing results

--- cuentas_bancarias.rb
+++ cuentas_bancarias.rb
@@ -1,10 +1,11 @@
 #Ejercicio 1
+
 class CuentaBancaria
     attr_accessor :saldo
     attr_reader :banco, :numero_de_cuenta
 
     def initialize(banco, numero_de_cuenta, saldo = 0.0)
-        raise ArgumentError, "Numero de cuenta solo debe contener numeros" if (numero_de_cuenta.to_i != Numeric)
+        raise ArgumentError, "Numero de cuenta solo debe contener numeros" if (numero_de_cuenta.count('a-z') > 0 || numero_de_cuenta.count('A-Z') > 0)
         @banco = banco
         @numero_de_cuenta = numero_de_cuenta
         @saldo = saldo
@@ -42,19 +43,52 @@ class Usuario
     end
 end
 
-cuenta1 = CuentaBancaria.new('Elite',12345,5000)
-cuenta2 = CuentaBancaria.new('Pro',6789,5000)
-cuenta3 = CuentaBancaria.new('Diamond',765456,5000)
+#Valores de prueba
+bd_cuentas = [CuentaBancaria.new('Elite','12345',5000), 
+              CuentaBancaria.new('Pro','6789',5000), 
+              CuentaBancaria.new('Diamond','765456',5000)]
 
-user1 = Usuario.new('Jorge',cuenta1)
-user2 = Usuario.new('Miguel',cuenta2)
+bd_usuarios = [Usuario.new('Jorge', bd_cuentas[0]), 
+               Usuario.new('Miguel', [bd_cuentas[1], bd_cuentas[2]])]
 
-puts user1.saldo_total
-puts user2.saldo_total
 
-puts "Transferencia"
+puts "Cuentas de prueba"
+bd_cuentas.count.times do |i|
+    puts "Cuenta #{i+1}: #{bd_cuentas[i].banco} - Nº #{bd_cuentas[i].numero_de_cuenta} - Saldo Actual: #{bd_cuentas[i].saldo}"    
+end
 
-user1.cuentas[0].transferir(user1.saldo_total, user1.cuentas[0])
+puts "\nUsuarios de prueba"
+bd_usuarios.count.times do |i|
+    puts "Usuario #{i+1}: #{bd_usuarios[i].nombre} \nCuentas:"
+    bd_usuarios[i].cuentas.each do |cuenta_test|
+        puts "#{cuenta_test.banco} - Nº #{cuenta_test.numero_de_cuenta} - Saldo Actual #{cuenta_test.saldo}"
+    end
+    puts "\n"
+end
 
-puts user1.saldo_total
-puts user2.saldo_total
+puts "Saldo Total"
+bd_usuarios.each do |usuario_test|
+    puts "Usuario: #{usuario_test.nombre} - #{usuario_test.saldo_total}"
+end
+
+puts "\nTransferencia"
+puts "Origen"
+puts "Usuario #{bd_usuarios[0].nombre} - Cuenta #{bd_usuarios[0].cuentas[0].banco} => Monto a Transferir #{bd_usuarios[0].cuentas[0].saldo}"
+puts "Destino"
+puts "Usuario #{bd_usuarios[1].nombre} - Cuenta #{bd_usuarios[1].cuentas[1].banco}"
+
+bd_usuarios[0].cuentas[0].transferir(bd_usuarios[0].saldo_total, bd_usuarios[1].cuentas[1])
+
+puts "\nResultado Transferencia - Saldos Totales"
+bd_usuarios.count.times do |i|
+    puts "#{bd_usuarios[i].nombre} => #{bd_usuarios[i].saldo_total}"
+end
+
+puts "\nDetalles de cuentas"
+bd_usuarios.count.times do |i|
+    puts "Usuario #{i+1}: #{bd_usuarios[i].nombre} \nCuentas:"
+    bd_usuarios[i].cuentas.each do |cuenta_test|
+        puts "#{cuenta_test.banco} - Nº #{cuenta_test.numero_de_cuenta} - Saldo Actual #{cuenta_test.saldo}"
+    end
+    puts "\n"
+end
